12c5163b7141c9ba449ec7f26efabb9c8a5e00b9
Steven Ling <robinhood1995@yahoo.com>
2024-09-12 21:03:25 -0400

    Added the Service Installer

---
+++ ProjectInstaller.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace EDI_KMC_Service
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.EDI_KMC_ServiceInstall = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // EDI_KMC_ServiceInstall
+            // 
+            this.EDI_KMC_ServiceInstall.Description = "FIFO File Processor for ESP/KMC";
+            this.EDI_KMC_ServiceInstall.ServiceName = "Kiwiplan KMC_EDI_Service";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.EDI_KMC_ServiceInstall});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller EDI_KMC_ServiceInstall;
+    }
+}
